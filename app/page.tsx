220ebe0a6f411eb7b604081767422b73e81cf2ab
PXXCXNT <pxxcxnt@icloud.com>
2025-09-20 05:33:44 +0900

    semaphore

--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,11 @@ import styles from "./page.module.css";
 export default function Home() {
   return (
     <main className={styles.main}>
+      <div className={styles.container}>
       <div className={styles.red} />
       <div className={styles.yellow} />
       <div className={styles.green} />
+      </div>
       </main>
   );
 }
