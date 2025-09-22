947eeda3873053d7206cc4fcfcbb5d7110f51a64
PXXCXNT <pxxcxnt@icloud.com>
2025-09-23 02:32:23 +0900

    add bubble

--- app/page.tsx
+++ app/page.tsx
@@ -4,10 +4,29 @@ export default function Home() {
   return (
     <main className={styles.main}>
       <div className={styles.container}>
-      <div className={styles.red} />
-      <div className={styles.yellow} />
-      <div className={styles.green} />
+        <div className={styles.box}>
+          <div className={styles.profileWrapper}>
+            <div className={styles.profileImage}>
+              <div className={styles.thoughtBubble}>
+                <div className={styles.bubbleContent}>
+                  <p>바지👖에☝️똥싼💩찬☀️</p>
+                </div>
+                <div className={styles.bubbleTail}>
+                  <span className={styles.bubble1}></span>
+                  <span className={styles.bubble2}></span>
+                  <span className={styles.bubble3}></span>
+                </div>
+              </div>
+              <img src="https://media.tenor.com/3zBRB7FbwDsAAAAM/groei-it-charlottexangie.gif" />
+            </div>
+          </div>
+          <div className={styles.light}>
+            <div className={styles.red} style={{ animationDelay: "0.1s" }} />
+            <div className={styles.yellow} style={{ animationDelay: "0.2s" }} />
+            <div className={styles.gray} style={{ animationDelay: "0.3s" }} />
+          </div>
+        </div>
       </div>
-      </main>
+    </main>
   );
 }
